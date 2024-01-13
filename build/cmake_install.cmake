f9ce24d5cbd0a86ea903ba8ec09417dc67ec9e77
Geetham Pasumarty <geetham.pasumarty@outlook.com>
2024-01-13 14:28:58 +0000

    turtle pose subscriber and twist publisher

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -132,6 +132,7 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/geetham/catkin_ws/build/gtest/cmake_install.cmake")
   include("/home/geetham/catkin_ws/build/beginner_tutorials/cmake_install.cmake")
+  include("/home/geetham/catkin_ws/build/rospy_tutorial/cmake_install.cmake")
 
 endif()
 
---
+++ build/rospy_tutorial/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "geometry_msgs;rospy;turtlesim".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "rospy_tutorial"
+PROJECT_SPACE_DIR = "/home/geetham/catkin_ws/devel"
+PROJECT_VERSION = "0.0.0"
